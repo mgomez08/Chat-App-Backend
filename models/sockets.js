d191f0a71dc4ff049eb4a1bcf6a09c8dc12d10f6
Marlon Gómez <marlonjulian15@gmail.com>
2021-08-23 11:56:08 -0500

    Add users-list socket event and use getUsers

--- models/sockets.js
+++ models/sockets.js
@@ -1,4 +1,8 @@
-const { userConnect, userDisconnect } = require("../controllers/sockets");
+const {
+  userConnect,
+  userDisconnect,
+  getUsers,
+} = require("../controllers/sockets");
 const { checkJWT } = require("../helpers/jwt");
 
 class Sockets {
@@ -9,16 +13,21 @@ class Sockets {
   socketEvents() {
     //On Connection
     this.io.on("connection", async (socket) => {
-      //Listen Event: newMessage
+      //UserConnect and check your JWT
       const [checked, uid] = checkJWT(socket.handshake.query["x-token"]);
       if (!checked) {
         console.log("Socket no identificado");
         return socket.disconnect();
       }
+      //User is connect
       await userConnect(uid);
       console.log("Cliente conectado ", uid);
+      //Emit all users connected
+      this.io.emit("users-list", await getUsers());
+      //UserDisconnect
       socket.on("disconnect", async () => {
         await userDisconnect(uid);
+        this.io.emit("users-list", await getUsers());
         console.log("Cliente desconectado", uid);
       });
     });
